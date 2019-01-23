d8ec5346d8cb752bd923f88baad7452da8722778
AthenaPK <athenapapkouf@gmail.com>
2019-01-23 23:16:44 +0100

    add input validation

--- birthday-gps.js
+++ birthday-gps.js
@@ -8,14 +8,14 @@ var googleMaps;
 function getInput(){
 	birthday1 = new Date(document.getElementById("birthday1").value);
 	birthday2 = new Date(document.getElementById("birthday2").value);
-	latitude = document.getElementById("latitude").value.match(/^\d{1,2}[.]/);
-	longitude =  document.getElementById("longitude").value.match(/^\d{1,2}[.]/);
+	latitude = document.getElementById("latitude").value;//.match(/^\d{1,2}[.]/);
+	longitude =  document.getElementById("longitude").value;//.match(/^\d{1,2}[.]/);
 }
 function findLocation(){
 	getInput();
 	validate();
 	if(validate()==true){
-	console.log("validation");
+	console.log("validated!");
 	console.log(birthday1);
 	var newLatitude = latitude + birthday1.toLocaleDateString('en-GB').split(/\W/).join("");
 	var newLongitude = longitude + birthday2.toLocaleDateString('en-GB').split(/\W/).join("");
@@ -24,21 +24,32 @@ function findLocation(){
 }
 
 function validate(){
-	if (!isFilled(latitude) || !isFilled(longitude) || !isFilled(birthday1) || !isFilled(birthday2)){
-			return false;
-	}else {
+	if (!isFilled(latitude) || !isFilled(longitude)){
+		console.log("Please enter coordinates")
+		return false;
+	}else if(isNaN(latitude) || isNaN(longitude)){
+		console.log("Please enter valid coordinates");
+		return false;
+	}else if (latitude > 90 || latitude < -90 || longitude < -180 || longitude > 180) {
+		console.log("Please enter valid coordinates");
+		return false;
+	}else if (!Date.parse(birthday1) || !Date.parse(birthday2)) {
+		console.log("Please enter a valid date");
+		return false;
+	}
+	else {
 		return true;
 	}
 }
 function isFilled(input){
-	if(input==null){
-		console.log("blank");
+	if(input==null || input == ""){
 		return false;
+		// TODO: return error message
 	}else{
-		console.log('true');
 		return true;
 	}
 }
+
 //TODO don't create new element, update link instead
 function printAnswer(){
 	var a = document.createElement('a');
